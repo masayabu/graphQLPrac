2a2689b3ea6925ea3178ae03f76e607ac12cc12f
masataka ohyabu <masa.yabu@gmail.com>
2022-11-10 12:10:08 +0900

    221110_ObjectNest

--- server.js
+++ server.js
@@ -4,20 +4,46 @@ const { buildSchema } = require('graphql');
 
 // スキーマ定義
 const schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
     type Query{
-        rollDice(numDice: Int!, numSides: Int): [Int]
+        getDie(numSides: Int): RandomDie
     }
 `);
 
-//リゾルバ関数
-const root = {
-    rollDice:({numDice, numSides}) => {
-        let output =[];
-        for(var i = 0; i < numDice; i++){
-            output.push(1 + Math.floor(Math.random()*(numSides ||6)));
+class RandomDie {
+    constructor(numSides){
+        this.numSides = numSides;
+    }
+
+    rollOnce(){
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({numRolls}){
+        let output = [];
+        for (var i = 0; i < numRolls; i++){
+            output.push(this.rollOnce());
         }
         return output;
     }
+}
+
+//リゾルバ関数
+const root = {
+    getDie:({numSides}) => {
+        return new RandomDie(numSides || 6);
+    }
+    // rollDice:({numDice, numSides}) => {
+    //     let output =[];
+    //     for(var i = 0; i < numDice; i++){
+    //         output.push(1 + Math.floor(Math.random()*(numSides ||6)));
+    //     }
+    //     return output;
+    // }
     // quoteOfTheDay:()=>{
     //     return Math.random() < 0.5 ? 'Take it easy':'Salvation lies within';
     // },
@@ -41,3 +67,13 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
+//test query　ここは、GraphQL 上記 endpointで打つところ
+// query{
+// 	getDie(numSides:3){
+//     numSides
+//     rollOnce
+//     roll(numRolls:5)
+//   }
+// }
